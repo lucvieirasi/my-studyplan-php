6dac7cd71c3eaec76595bfebe896a87cda583b69
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-03 12:43:08 -0300

    Exercise logic programming in PHP.

--- basico/desafio.php
+++ basico/desafio.php
@@ -1,7 +1,7 @@
 <div class="titulo">Desafio</div>
 
 <ul>
-    <li>1 + 1 =  <?= "Legal" ?></li>
-    <li>4 + 4 = ? <?= ?></li>
-    <li>8 + 8 = ? <?= ?></li>
+    <li>1 + 1 =  <?= " 2" ?></li>
+    <li>4 + 4 =  <?= " 8" ?></li>
+    <li>8 + 8 =  <?= " 16" ?></li>
 </ul>
