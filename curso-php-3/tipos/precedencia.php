fe3800ae84ddcd366d5594a1016a64c64aa6ad25
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-04 15:59:00 -0300

    Challenge precedence resolved.

---
+++ curso-php-3/tipos/precedencia.php
@@ -0,0 +1,13 @@
+<div class="titulo">Desafio Precedencia</div>
+
+<?php
+echo '<p>1. Qual valor será impresso?</p>';
+echo 2 + 5 * 3 + (12 / 6) / (-8 + 2 ** 3);
+
+
+echo '<p>2. Qual expressao imprime o valor 100?</p>';
+echo 'a)', (1 + 2) * 20 - 15, '<br>';
+echo 'b)', 4 * 5 ** 2, '<br>';
+echo 'c)', 2 ** 3 ** 4 / 1e25, '<br>';
+echo 'd)', 3 + (3 * 8) / 2 - 3, '<br>';
+
---
+++ curso-php-3/tipos/resultadodesafio.php
@@ -0,0 +1,12 @@
+<div class="titulo">Desafio Precedencia</div>
+
+<?php
+echo '<p>1. Qual valor será impresso?</p>';
+echo 'O valor impresso será INF(Infinito, quando um número nao tem fim).';
+
+
+echo '<p>2. Qual expressao imprime o valor 100?</p>';
+
+echo 'A alternativa correta é B, pois `4 * 5 ** 2` resulta em 100.';
+
+
