fa156ed9f4336e88131bee070e3df4b9b5ba3867
Luciano Vieira <contato@lucianovieirapro.com>
2022-06-08 16:14:25 -0300

    String and types.

---
+++ curso-php-3/tipos/string.php
@@ -0,0 +1,18 @@
+<div class="titulo">Tipo String</div>
+
+<?php
+echo 'Eu sou um string';
+echo '<br>';
+var_dump('Eu também');
+echo '<br>';
+
+// Concatenacao
+
+echo 'Nos tambem' . ' Somos';
+echo '<br>', 'Tambem aceito', ' virgulas';
+
+echo "'Teste '" . "'Teste'";
+
+print("<br> Tambem existe a funcao print");
+echo '<br>' . strtoupper('Tudo Maiusculo');
+echo '<br>' . strtolower('Tudo Minusculo');
